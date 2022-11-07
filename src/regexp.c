a6fa444148514a89daf80d21c20aae66c8fdc10c
Jaakko Keränen <jaakko.keranen@iki.fi>
2022-11-07 23:28:18 +0200

    RegExp: Fixed checking for results without captures
    The return value from `pcre2_match` was incorrectly checked.

--- src/regexp.c
+++ src/regexp.c
@@ -51,7 +51,7 @@ struct Impl_RegExp {
 };
 
 iRegExp *new_RegExp(const char *pattern, enum iRegExpOption options) {
-    PCRE2_SIZE opts = PCRE2_UTF | PCRE2_UCP;
+    uint32_t opts = PCRE2_UTF | PCRE2_UCP | PCRE2_NO_UTF_CHECK;
     if (options & caseInsensitive_RegExpOption) {
         opts |= PCRE2_CASELESS;
     }
@@ -88,7 +88,7 @@ iBool match_RegExp(const iRegExp *d, const char *subject, size_t len, iRegExpMat
     pcre2_match_data *matchData = pcre2_match_data_create_from_pattern(d->re, NULL);
     int rc = pcre2_match(d->re, (PCRE2_SPTR) subject, (PCRE2_SIZE) len, (PCRE2_SIZE) match->pos,
                          0, matchData, NULL);
-    if (rc > 1) {
+    if (rc >= 1) {
         PCRE2_SIZE *output = pcre2_get_ovector_pointer(matchData);
         uint32_t count = pcre2_get_ovector_count(matchData);
         iRangei *mrange = &match->range;
@@ -99,7 +99,7 @@ iBool match_RegExp(const iRegExp *d, const char *subject, size_t len, iRegExpMat
         match->pos = match->range.end;
     }
     pcre2_match_data_free(matchData);
-    return rc > 1;
+    return rc >= 1;
 }
 
 #else /* PCRE version 8 */
